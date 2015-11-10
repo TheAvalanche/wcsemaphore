1149bdc703a861aa77672b09685b650eb7799f4f
Alex Kartishev <alexkartishev@gmail.com>
2015-11-10 10:45:37 +0200

    ids

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -8,7 +8,40 @@ import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
 
-    def wcStatuses = [new WcDetails(id: 'wc1-1', status: free), new WcDetails(id: 'wc2-1', status: free), new WcDetails(id: 'wc3-1', status: free)]
+    def wcStatuses = [
+            new WcDetails(id: 'wc1-1', status: free),
+            new WcDetails(id: 'wc1-2', status: free),
+            new WcDetails(id: 'wc1-3', status: free),
+            new WcDetails(id: 'wc1-4', status: free),
+            new WcDetails(id: 'wc1-5', status: free),
+            new WcDetails(id: 'wc1-6', status: free),
+            new WcDetails(id: 'wc1-7', status: free),
+            new WcDetails(id: 'wc1-8', status: free),
+            new WcDetails(id: 'wc1-9', status: free),
+            new WcDetails(id: 'wc1-10', status: free),
+            new WcDetails(id: 'wc1-11', status: free),
+            new WcDetails(id: 'wc1-12', status: free),
+            new WcDetails(id: 'wc1-13', status: free),
+            new WcDetails(id: 'wc2-1', status: free),
+            new WcDetails(id: 'wc2-2', status: free),
+            new WcDetails(id: 'wc2-3', status: free),
+            new WcDetails(id: 'wc2-4', status: free),
+            new WcDetails(id: 'wc2-5', status: free),
+            new WcDetails(id: 'wc2-6', status: free),
+            new WcDetails(id: 'wc2-7', status: free),
+            new WcDetails(id: 'wc2-8', status: free),
+            new WcDetails(id: 'wc2-9', status: free),
+            new WcDetails(id: 'wc3-1', status: free),
+            new WcDetails(id: 'wc3-2', status: free),
+            new WcDetails(id: 'wc3-3', status: free),
+            new WcDetails(id: 'wc3-4', status: free),
+            new WcDetails(id: 'wc3-5', status: free),
+            new WcDetails(id: 'wc3-6', status: free),
+            new WcDetails(id: 'wc3-7', status: free),
+            new WcDetails(id: 'wc3-8', status: free),
+            new WcDetails(id: 'wc3-9', status: free),
+            new WcDetails(id: 'wc3-10', status: free),
+    ]
     handlers {
 
         get {
