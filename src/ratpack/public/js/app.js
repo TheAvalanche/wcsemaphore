1858f6259de59844e167ed4693c020733938534d
Alex Kartishev <alexkartishev@gmail.com>
2015-11-10 15:39:16 +0200

    free counter

--- src/ratpack/public/js/app.js
+++ src/ratpack/public/js/app.js
@@ -26,6 +26,7 @@ function doAsyncThing() {
                 $("#" + obj.id).removeClass();
                 $("#" + obj.id).addClass(obj.status);
             }
+            calculateFree(data);
         },
         complete: function () {
             setTimeout(function () {
@@ -36,3 +37,32 @@ function doAsyncThing() {
 }
 
 doAsyncThing();
+
+function calculateFree(data) {
+    var floor1Count = 0;
+    var floor2Count = 0;
+    var floor3Count = 0;
+    for (var key in data) {
+        if (data[key].status == "opened") {
+            if (stringStartsWith(data[key].id, "wc1")) {
+                floor1Count++;
+            } else if (stringStartsWith(data[key].id, "wc2")) {
+                floor2Count++;
+            } else if (stringStartsWith(data[key].id, "wc3")) {
+                floor3Count++;
+            }
+        }
+
+    }
+    $("#counter1").text(floor1Count);
+    $("#counter2").text(floor2Count);
+    $("#counter3").text(floor3Count);
+}
+
+function stringStartsWith (str, prefix) {
+    if (str.length < prefix.length)
+        return false;
+    for (var i = prefix.length - 1; (i >= 0) && (str[i] === prefix[i]); --i)
+        continue;
+    return i < 0;
+}
